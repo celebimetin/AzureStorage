88502dfb57d057d260a4b06b3fac12aa6b6022dd
Metin Çelebi <metincelebi5534@gmail.com>
2022-10-15 23:02:28 +0300

    ConsoleApp ortamında Azure Queue Storage Tamamlandı

---
+++ QueueConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using AzureStorageLibrary;
+using AzureStorageLibrary.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace QueueConsoleApp
+{
+    internal class Program
+    {
+        private async static Task Main(string[] args)
+        {
+            ConnectionStrings.AzureStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=udemyrealstorageaccount;AccountKey=kCxkM3J1A4DQIhoWDZVXr0JbXIaiv43oBDsjXqzEHYGJ3piMPRRXJpov1zFzjaniERUKEUxBZhTG+AStDsIJHA==;EndpointSuffix=core.windows.net";
+
+            AzureQueue azureQueue = new AzureQueue("testkuyruk");
+
+            //string base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes("mcelebi"));
+            //azureQueue.SendMessageAsync(base64).Wait();
+
+            var message = azureQueue.RetrieveNextMessageAsync().Result;
+            //string text = Encoding.UTF8.GetString(Convert.FromBase64String(message.MessageText));
+
+            //Console.WriteLine("Mesaj " + text);
+
+            await azureQueue.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+
+            Console.WriteLine("Mesaj silindi");
+        }
+    }
+}
